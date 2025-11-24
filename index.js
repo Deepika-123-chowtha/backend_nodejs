416893b90a7afa34cb80b90019949715f5b68216
Deepika-123-chowtha <chowthadeepika@gmail.com>
2025-11-24 16:43:14 +0530

    made changes for PORT and url with homepage

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const productRoutes=require('./routes/productRoutes')
 
 const path=require('path');
 const app=express()
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 dotEnv.config();
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected successfully!"))
@@ -23,7 +23,7 @@ app.use('/uploads',express.static('uploads'));
 app.listen(PORT, () => {
     console.log(`server started and running at ${PORT}`);
 });
-app.use('/home', (req, res) => {
+app.use('/', (req, res) => {
     res.send("<h1> Welcome to DELIVERO");
 })
         
